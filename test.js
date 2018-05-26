b562281f19896f97af2aa37776a790049a962854
jjy951106 <jjy951106@gmail.com>
2018-05-26 23:32:59 +0900

    카카오톡 연동

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
 var express = require('express');
+
+var http = require('http');
+
+var app = express();
+
+app.get('/keyboard', function(req, res) {
+	
+	var data = {
+		'type': 'buttons',
+		'button': ['안녕']
+	};
+	
+	res.json(data);
+});
+
+http.createServer(app).listen(9090, function(){
+	console.log('서버 실행');
+});
