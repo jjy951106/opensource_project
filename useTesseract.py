d9c586189d78a17c9a7f20a6dc328dd51a97d128
kdogyun <kbomin98@naver.com>
2018-06-03 18:03:06 +0900

    tesseract 이용한 OCR 구현 - 한글빼고 모든 언어가 다 되는데 한글은 안된다.. - 아무리 오류를 해결하려고 해봐도 되지않는다.. - 나만 에러가 뜨는것 같다;; - 이 오류 해결을 위해 몇번을 깔았다 지웠다.. 이방법 저방법 다 해보면서   이틀을 날렸으나 해결하지 못했다...
    <에러내용>
    Traceback (most recent call last):
      File "F:\Users\dogyun\Desktop\까치\useTesseract.py", line 16, in <module>
        ocr_tesseract()
      File "F:\Users\dogyun\Desktop\까치\useTesseract.py", line 12, in ocr_tesseract
        print(pytesseract.image_to_string(im, lang='kor', config=tessdata_dir_config))
      File "C:\Users\dogyun\AppData\Local\Programs\Python\Python36-32\lib\site-packages\pytesseract\pytesseract.py", line 286, in image_to_string
        return run_and_get_output(image, 'txt', lang, config, nice)
      File "C:\Users\dogyun\AppData\Local\Programs\Python\Python36-32\lib\site-packages\pytesseract\pytesseract.py", line 194, in run_and_get_output
        run_tesseract(**kwargs)
      File "C:\Users\dogyun\AppData\Local\Programs\Python\Python36-32\lib\site-packages\pytesseract\pytesseract.py", line 170, in run_tesseract
        raise TesseractError(status_code, get_errors(error_string))
    pytesseract.pytesseract.TesseractError: (3221225477, '')

---
+++ useTesseract.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import pytesseract
+
+tessdata_dir_config = '--tessdata-dir "C:\\Program Files (x86)\\Tesseract-OCR\\tessdata"'
+tesseract_path = 'C:\\Program Files (x86)\\Tesseract-OCR'
+pytesseract.pytesseract.tesseract_cmd = tesseract_path + '\\tesseract'
+
+
+def ocr_tesseract():
+    image_file = 'F:\\Users\\dogyun\\Desktop\\까치\\scannedImage.jpg'
+    im = Image.open(r'F:\\Users\\dogyun\\Desktop\\까치\\scannedImage.jpg')
+    print(pytesseract.image_to_string(im, lang='-l kor+eng', config=tessdata_dir_config))
+    im.show()
+
+if __name__ == '__main__':
+    ocr_tesseract()
+    
